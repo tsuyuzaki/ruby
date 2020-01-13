34155c2594fe9a97feadd7973d8884da5982895c
tsuyuzaki <norihiko.tsuyuzaki@mazrica.com>
2020-01-14 01:48:00 +0900

    Added ch13

---
+++ tutorial/sample_app_ch13/test/integration/users_index_test.rb
@@ -0,0 +1,52 @@
+require 'test_helper'
+
+class UsersIndexTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @admin = users(:michael)
+    @non_admin = users(:archer)
+  end
+
+  test "index including pagination" do
+    log_in_as(@admin)
+    get users_path
+    assert_template 'users/index'
+    assert_select 'div.pagination', count: 2
+    User.paginate(page: 1).each do |user|
+      validate_user_ref(user)
+    end
+  end
+
+  test "index as admin including pagination and delete links" do
+    log_in_as(@admin)
+    get users_path
+    assert_template 'users/index'
+    assert_select 'div.pagination'
+    first_page_of_users = User.paginate(page: 1)
+    first_page_of_users.each do |user|
+      validate_user_ref(user)
+      next unless user.activated?
+      unless user == @admin
+        assert_select 'a[href=?]', user_path(user), text: 'delete'
+      end
+    end
+    assert_difference 'User.count', -1 do
+      delete user_path(@non_admin)
+    end
+  end
+
+  test "index as non-admin" do
+    log_in_as(@non_admin)
+    get users_path
+    assert_select 'a', text: 'delete', count: 0
+  end
+
+  private
+  def validate_user_ref(user)
+    if user.activated?
+      assert_select 'a[href=?]', user_path(user), text: user.name
+    else
+      assert_select 'a[href=?]', user_path(user), count: 0
+    end
+  end
+end
