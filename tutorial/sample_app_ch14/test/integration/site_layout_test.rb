0bf9d11192dd86060f3b4f23d0a4d91a40416a5c
tsuyuzaki <norihiko.tsuyuzaki@mazrica.com>
2020-01-29 01:54:08 +0900

    ch14 追加

---
+++ tutorial/sample_app_ch14/test/integration/site_layout_test.rb
@@ -0,0 +1,50 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:michael)
+  end
+
+  test "not login layout links" do
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+
+    assert_select "a[href=?]", help_path
+    get help_path
+    assert_select "title", full_title("Help")
+
+    assert_select "a[href=?]", about_path
+    get about_path
+    assert_select "title", full_title("About")
+
+    assert_select "a[href=?]", contact_path
+    get contact_path
+    assert_select "title", full_title("Contact")
+
+    assert_select "a[href=?]", login_path
+    get signup_path
+    assert_select "title", full_title("Sign up")
+  end
+
+  test "login layout links" do
+    log_in_as(@user)
+    get root_path
+    assert_template "static_pages/home"
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
+    assert_select "a[href=?]", logout_path
+  end
+
+  test "count relationships" do
+    log_in_as(@user)
+    get root_path
+    assert_match @user.active_relationships.count.to_s, response.body
+    assert_match @user.passive_relationships.count.to_s, response.body
+  end
+end
